6296f375336b8a36bba039c7f27ce2f32eb522ff
Tanya Agrahari <rg96958227@gmail.com>
2026-01-01 21:47:48 +0530

    python all  fundamental details

---
+++ main.py
@@ -0,0 +1,58 @@
+
+
+print("hello tanya ")
+print(5)
+print (" hey i a good girl")
+
+# variable
+a= 1324
+print (a);
+b ="Tanya"
+print (b);
+print (" type of a is ", type(a))
+#  operator
+
+print(5+3)
+print(5-3)
+print(5*3)
+print(5/3)
+print(5//3)
+print(5**3)
+print(5%3)
+#  type casting
+print (int(5.9))
+print (float(5))
+print (str(5))
+#  implicit type casting
+a=5
+b=2.5
+c = a+b
+print ("value of c is ", c)
+print("type of a is ", type(c))
+#  input function 
+name = input()
+print (" my name is ", name)
+x= input(" enter first value ")
+y= input(" enter second value ")
+print (" sum of x and y is ", int(x)+int(y))
+# string
+
+
+name ="harry"
+freind ="tanya"
+anotherFriend ="shivam raj"
+print (name + " is a friend of  " + freind+ "missing " + anotherFriend)
+#  accessing characters
+print (name[0])
+print (name[1])
+print (name[2])
+print (name[3])
+print (name[4])
+# slicing
+print (name[0:4])
+print (name[1:4])
+length = len(name)
+print (length)
+
+
+
