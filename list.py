d0370cbd345c98961a40511f28456f6894a6d465
Tanya Agrahari <rg96958227@gmail.com>
2026-01-04 02:07:02 +0530

    List in python

---
+++ list.py
@@ -0,0 +1,24 @@
+marks=[85, 90, 78, 92, 88]
+print("Marks List:", marks)
+# print(type(marks))
+# print ("First mark:", marks[0])
+# print("Last mark:", marks[-1])
+# # list methods
+# marks.append(95)
+# print("After appending 95:", marks)
+# marks.remove(78)
+# print("After removing 78:", marks)
+# marks.sort()
+# print("After sorting:", marks)
+# marks.reverse()
+# print("After reversing:", marks)
+# m= marks.copy()
+# print("Copied list:", m)
+m=[1,2,3,4,5]
+k=marks+m
+print("Concatenated List:", k)
+
+
+
+
+
