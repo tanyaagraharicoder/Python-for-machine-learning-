e8a38b87c5ecb62f52e69bbb20f2f22e615a0281
Tanya Agrahari <rg96958227@gmail.com>
2026-01-07 23:31:18 +0530

    more details about python

---
+++ dictionary.py
@@ -0,0 +1,73 @@
+
+# dic2={
+#     344:"tanya",
+#     456:"shivam",
+#     789:"shivanya",
+
+# }
+# print(dic2[344])
+
+# info ={
+#     "name":"tanya",
+#     "age":20,
+#     "city":"delhi",
+#     "is_student":True,
+# }
+# print(info["name"])
+# print(info.get("age"))
+# print(info.keys())
+# print(info.values())
+# print(info.items())
+
+# ep1={122:45, 123:67, 124:89}
+# ep2={125:90, 126:78, 127:56}
+# ep1.update(ep2)
+# print(ep1)
+# ep1.clear()
+# print(ep1)
+# ep1.pop(122)
+# print(ep1)
+
+# for loop with else in python
+# for i in range(5):
+#     print(i)
+# else:
+#     print("loop is ended")
+# excepio handling
+
+# a=input("enter the number")
+# print("multication table of",a  )
+# try:
+#     for i in range(1,11):
+#         print(f"{a} x {i} = {int(a)*i}")
+# except Exception as e:
+#     print("invalid input",e)
+
+# print(" important codes")
+
+
+# finally - 
+# l = [1, 2, 3, 4, 5]
+
+# try:
+#     i = int(input("Enter the index: "))
+#     print(l[i])
+
+# except IndexError:
+#     print("Index out of range")
+
+# finally:
+#     print("Execution completed")
+
+#  raising custom exception
+# a=(int(input("enter any value between 5 and 9: ")))
+
+# if(a<5 or a>9):
+#     raise ValueError("value should be between 5 and 9 ")
+
+# if else in one line 
+# print("a") if(a>b) else print("b")  
+
+
+
+
