bb259c427c4cf04ac6c230022ac735ffc9dc2ac7
Tanya Agrahari <rg96958227@gmail.com>
2026-01-11 16:56:08 +0530

    abot import  work in python and os import

---
+++ tanya.py
@@ -0,0 +1,8 @@
+def welcome():
+    print("welcome to tanya.py")
+welcome()    
+print(__name__)
+
+
+if __name__ == "__main__":
+    welcome()
---
+++ tut4.py
@@ -0,0 +1,20 @@
+# import math
+# import math as m
+# print(m.sqrt(16))  # Example usage of math module
+# print (dir(m))
+
+
+# import tanya
+
+# tanya.welcome()
+
+x=4
+print (x)
+
+def hello():
+    x=89
+    print(x)
+    print("Hello, World!")
+
+hello()
+
