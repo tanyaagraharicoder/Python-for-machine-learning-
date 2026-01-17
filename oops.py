6feee6fa838cd6b17cf7afcf3638bfdde25e93e9
Tanya Agrahari <rg96958227@gmail.com>
2026-01-17 23:21:00 +0530

    constructors in python

--- oops.py
+++ oops.py
@@ -1,11 +1,26 @@
+# class Person:
+#     name="Harry"
+#     occupation= "Software Developer"
+#     netwoth=10
+
+#     def info(self):
+#         print(f"{self.name} is a {self.occupation}")
+
+# a=Person()
+# print(a.name);  
+# a.name= "tanya"
+# print(a.info()) 
+
+
 class Person:
-    name="Harry"
-    occupation= "Software Developer"
-    netwoth=10
+    def __init__(self,n,o):
+        print("Hey i m a person")
+        self.name=n
+        self.occ= o
 
     def info(self):
-        print(f"{self.name} is a {self.occupation}")
+        print(f"{self.name} is a {self.occ}")    
+        
 
-a=Person()
-print(a.name);  
-print(a.info()) 
+a=Person( "tanya","dev")
+a.info()
