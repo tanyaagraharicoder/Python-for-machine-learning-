0e9c1cc18c080c6e420f07e4517bdcfe11ed2ea8
Tanya Agrahari <rg96958227@gmail.com>
2026-01-21 17:51:57 +0530

    getter and setter in python

--- oops.py
+++ oops.py
@@ -28,20 +28,62 @@
 # decorators
 
 
-def greet(fx):
-    def mfx():
-        print("Good morning")
-        fx()
-        print("Have a nice day")
+# def greet(fx):
+#     def mfx():
+#         print("Good morning")
+#         fx()
+#         print("Have a nice day")
 
-    return mfx
-
-
-# @greet
-def hello():
-    print("hello world")
+#     return mfx
 
 
-# hello()
+# # @greet
+# def hello():
+#     print("hello world")
 
-greet(hello)
+
+# # hello()
+
+# greet(hello)
+
+
+
+
+#  without getter and setter 
+
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+p= Person("tanya",20)
+print(p.age)  # direct access to age attribute
+
+p.age= -5  # direct modification to age attribute
+print(p.age)  # incorrect age value
+
+# with getter and setter
+
+
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.set_age(age)
+
+    def get_age(self):
+        return self._age
+
+    def set_age(self,age):
+        if age < 0:
+            print("Age cannot be negative. Setting age to 0.")
+            self._age = 0
+        else:
+            self._age = age
+
+
+
+
+p= Person("tanya",20)
+print(p.get_age())  # access age via getter
+p.set_age(-5)  # attempt to set a negative age
+print(p.get_age())  # age is now corrected to 0
