96cd9a46914b661b041e8ff57ac633a8300c7305
Tanya Agrahari <rg96958227@gmail.com>
2026-01-21 19:47:35 +0530

    inheritance in python

--- oops.py
+++ oops.py
@@ -51,39 +51,82 @@
 
 #  without getter and setter 
 
-class Person:
-    def __init__(self,name,age):
+# class Person:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+
+# p= Person("tanya",20)
+# print(p.age)  # direct access to age attribute
+
+# p.age= -5  # direct modification to age attribute
+# print(p.age)  # incorrect age value
+
+# # with getter and setter
+
+
+# class Person:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.set_age(age)
+
+#     def get_age(self):
+#         return self._age
+
+#     def set_age(self,age):
+#         if age < 0:
+#             print("Age cannot be negative. Setting age to 0.")
+#             self._age = 0
+#         else:
+#             self._age = age
+
+
+
+
+# p= Person("tanya",20)
+# print(p.get_age())  # access age via getter
+# p.set_age(-5)  # attempt to set a negative age
+# print(p.get_age())  # age is now corrected to 0
+
+
+
+# inheritance in python 
+
+class Employee:
+    def __init__(self,name,salary):
         self.name=name
-        self.age=age
+        self.salary= salary
 
-p= Person("tanya",20)
-print(p.age)  # direct access to age attribute
-
-p.age= -5  # direct modification to age attribute
-print(p.age)  # incorrect age value
-
-# with getter and setter
+    def info(self):
+        print(f"{self.name} earns {self.salary} per month")
 
 
-class Person:
-    def __init__(self,name,age):
-        self.name=name
-        self.set_age(age)
+class Developer(Employee):
+    def __init__(self,name,salary,lang):
+        super().__init__(name,salary)
+        self.lang= lang
 
-    def get_age(self):
-        return self._age
+    def info(self):
+        super().info()
+        print(f"{self.name} works with {self.lang}")
 
-    def set_age(self,age):
-        if age < 0:
-            print("Age cannot be negative. Setting age to 0.")
-            self._age = 0
-        else:
-            self._age = age
+
+d= Developer("tanya",5000,"python")
+d.info()
+e= Employee("harry",7000)
+e.info()
 
 
 
 
-p= Person("tanya",20)
-print(p.get_age())  # access age via getter
-p.set_age(-5)  # attempt to set a negative age
-print(p.get_age())  # age is now corrected to 0
+
+
+
+
+
+
+
+
+
+
+
