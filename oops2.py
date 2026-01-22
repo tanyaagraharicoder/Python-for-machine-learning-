dddb7446115d21373990f3c4aa9cc89a9b4f8f92
Tanya Agrahari <rg96958227@gmail.com>
2026-01-22 14:58:40 +0530

    operator overloading

--- oops2.py
+++ oops2.py
@@ -49,6 +49,26 @@ class Dog(Animal):
 d= Dog()
 d.sound()  # Output: Dog barks
 
+# operator overloading 
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __str__(self):
+        return f"Vector({self.x}, {self.y})"
+
+v1 = Vector(2, 3)
+v2 = Vector(4, 5)   
+v3 = v1 + v2
+print(v3)
+# Output: Vector(6, 8)
+# Output: This is Parent class
+
 
 
 
