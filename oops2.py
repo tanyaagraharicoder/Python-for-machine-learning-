ba85dc5f87c27302488e841698dfca812082108e
Tanya Agrahari <rg96958227@gmail.com>
2026-01-22 11:29:05 +0530

    super keyWord in python

--- oops2.py
+++ oops2.py
@@ -1,16 +1,44 @@
-x=(1, 2,3)
-print(dir(x))
-print(x.__add__)
+# x=(1, 2,3)
+# print(dir(x))
+# print(x.__add__)
 
 
-# dict method 
+# # dict method 
 
-class person:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-p1= person("tanya",20)
-print(p1.__dict__)
+# class person:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+# p1= person("tanya",20)
+# print(p1.__dict__)
 
-print (help(person))
+# print (help(person))
+
+class Parent:
+    def show(self):
+        print("This is Parent class")
+
+class Child(Parent):
+    def show_child(self):
+        super().show()   # call parent method
+        print("This is Child class")
+c= Child()
+c.show_child()
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)   # call parent constructor
+        self.breed = breed
+d = Dog("Buddy", "Golden Retriever")
+print(d.name)  # Output: Buddy
+
+
+
+
+
+
 
